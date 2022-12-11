253144ae1b14cc001d94643c6a260b27747ba95a
x90c(id: tf8, lamagra) <97133739+x90hack@users.noreply.github.com>
2022-12-12 05:02:32 +0900

    Add files via upload

---
+++ file.py
@@ -0,0 +1,34 @@
+
+import os
+
+def makeFile(fileName, message, mode):
+    a=open(fileName, mode)
+    a.write(message)
+    a.close()
+
+def openFile(fileName):
+    b=open(fileName, "r")
+    lines = b.readlines()
+    for line in lines:
+        print(line)
+    b.close()
+
+os.system("del fileFirst.txt; del fileSecond.txt")
+makeFile("fileFirst.txt", "This is my first file1\n", "w")
+makeFile("fileFirst.txt", "This is my first file2\n", "w")
+makeFile("fileFirst.txt", "This is my first file3\n", "w")
+makeFile("fileSecond.txt", "This is my second file1\n", "a")
+makeFile("fileSecond.txt", "This is my second file2\n", "a")
+makeFile("fileSecond.txt", "This is my second file3\n", "a")
+
+print("write fileFirst.txt")
+print("--------------------------------------")
+openFile("fileFirst.txt")
+print("--------------------------------------")
+
+print("\n")
+
+print("write secondFile.txt")
+print("--------------------------------------")
+openFile("fileSecond.txt")
+print("--------------------------------------")
